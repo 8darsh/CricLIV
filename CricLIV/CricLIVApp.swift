912b0645194594b8c56a3d14f4da684868563471
Adarsh <adarsh232002@gmail.com>
2024-06-18 01:05:03 +0530

    Initial Commit

---
+++ CricLIV/CricLIVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricLIVApp.swift
+//  CricLIV
+//
+//  Created by Adarsh Singh on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CricLIVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
